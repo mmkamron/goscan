89416a7b1a7036794e7505ba01bddf5967672917
Kamron Mirsamatov <kronusk2@gmail.com>
2022-04-18 22:48:03 +0200

    top 20 ports

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func scan_port(hostname string, port int) scan_result {
 	result := scan_result{Port: port}
 	address := hostname + ":" + strconv.Itoa(port)
 	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
-	
+
 	if err != nil {
 		result.State = "Closed or filtered"
 		return result
@@ -28,10 +28,13 @@ func scan_port(hostname string, port int) scan_result {
 
 func initial_scan(hostname string) []scan_result {
 
+    ports := [20]int{21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 143, 443, 445, 993, 995, 1723, 3306, 3389, 5900, 8080}
+
+
 	var results []scan_result
 
-	for i := 0; i <= 1024; i++ {
-		results = append(results, scan_port(hostname, i))
+	for _, s := range ports {
+		results = append(results, scan_port(hostname, s))
 	}
 
 	return results
@@ -39,9 +42,9 @@ func initial_scan(hostname string) []scan_result {
 
 func main() {
 	var host string
-	fmt.Println("Port scanner by mmkamron")
-	fmt.Println("Enter an ip address or url to scan: ")
-	fmt.Scanln(&host)
+	fmt.Println("Port Scanner by mmkamron")
+	fmt.Printf("Enter an ip address or url to scan: ")
+	fmt.Scan(&host)
 	op := initial_scan(host)
 	fmt.Println(op)
 }
